d4032165859cd0f9c43c24d0c34775db8edd05a8
noel <68936@naver.com>
2021-01-15 01:23:26 +0900

    클래스 존재 이유와 기본형식

--- src/CodingEverybody/ProjectOOP/MyOOP.java
+++ src/CodingEverybody/ProjectOOP/MyOOP.java
@@ -1,41 +1,30 @@
 package CodingEverybody.ProjectOOP;
 
-
-public class MyOOP {
+class Print{
     public static String delimiter = "";
+    public static void A() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+    public static void B() {
+        System.out.println(delimiter);
+        System.out.println("B");
+        System.out.println("B");
+    }
+}
+public class MyOOP {
     public static void main(String[] args) {
+        Print.delimiter = "----";
+        Print.A();
+        Print.A();
+        Print.B();
+        Print.B();
 
-        delimiter = "----";
-        //.. 1억줄이 있다.
-        PrintA(); //m 우클릭 - Refactor - Extract Method
-        //.. 1억줄
-        PrintA();
-        // ..
-        PrintB();
-        PrintB();
-
-        delimiter = "****";
-        PrintA();
-        PrintA();
-        PrintB();
-        PrintB();
-
-        //m 구분자를 다르게 하고싶다면?
-        //m 방법1. 각각의 메소드에 파라미터 즉, 매게변수를 주는 것
-        //m 방법2. 전역변수를 통해서 사용
-        //m 하지만 위 방법들은 코드가 많아진다면 정말 비효율적임
-        //m 따라서 클래스를 사용!
-
-    }
-
-    private static void PrintA() {
-        System.out.println(delimiter); // 정말 중요한 코드라고 가정
-        System.out.println("A"); // 서로 연관된 로직
-        System.out.println("A"); // 서로 연관된 로직
-    }
-    private static void PrintB() {
-        System.out.println(delimiter); // 정말 중요한 코드라고 가정
-        System.out.println("A"); // 서로 연관된 로직
-        System.out.println("A"); // 서로 연관된 로직
+        Print.delimiter = "****";
+        Print.A();
+        Print.A();
+        Print.B();
+        Print.B();
     }
 }
