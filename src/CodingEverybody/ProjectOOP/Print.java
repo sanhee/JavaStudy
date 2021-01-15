f84ea52f66195866dc7eeda863730465a5225c26
noel <68936@naver.com>
2021-01-16 02:06:54 +0900

    클래스 분리

---
+++ src/CodingEverybody/ProjectOOP/Print.java
@@ -0,0 +1,19 @@
+package CodingEverybody.ProjectOOP;
+
+class Print{
+
+    //m 자바) 파일이름 -> 파일 이름의 클래스에서 main 메소드를 찾아서 실행하도록 약속 되어있음.
+    //m 클래스 안 변수와 메소드를 멤버라고 한다.
+
+    public static String delimiter = "";
+    public static void A() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+    public static void B() {
+        System.out.println(delimiter);
+        System.out.println("B");
+        System.out.println("B");
+    }
+}
