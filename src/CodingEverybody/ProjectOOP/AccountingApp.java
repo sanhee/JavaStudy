12082f59969e7f883cc45e7cd17b2bec7a9befe1
noel <68936@naver.com>
2021-01-16 15:57:48 +0900

    Accounting 클래스화

--- src/CodingEverybody/ProjectOOP/AccountingApp.java
+++ src/CodingEverybody/ProjectOOP/AccountingApp.java
@@ -1,25 +1,28 @@
 package CodingEverybody.ProjectOOP;
 
-public class AccountingApp {
+class Accounting{
     // 공급가액
-    public static double valueOfSupply = 10000.0;
+    public static double valueOfSupply;
     // 부가가치세율
     public static double vatRate = 0.1;
 
     public static double getVAT() {
         return valueOfSupply * vatRate;
     }
-
     public static double getTotal() {
         return valueOfSupply + getVAT();
     }
+}
+
+public class AccountingApp {
+
+    //m 연관된 메소드끼리 그룹화
 
     public static void main(String[] args) {
+        Accounting.valueOfSupply = 10000.0;
 
-        System.out.println("Value of supply : " + valueOfSupply);
-        System.out.println("VAT : " + getVAT());
-        System.out.println("Total : " + getTotal());
-
+        System.out.println("Value of supply : " + Accounting.valueOfSupply);
+        System.out.println("VAT : " + Accounting.getVAT());
+        System.out.println("Total : " + Accounting.getTotal());
     }
-
 }
