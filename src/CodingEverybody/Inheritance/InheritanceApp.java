c5d868c00afa9394d1656ea9b4a5ae23f582d632
noel <68936@naver.com>
2021-01-16 22:01:14 +0900

    this, super 실습

--- src/CodingEverybody/Inheritance/InheritanceApp.java
+++ src/CodingEverybody/Inheritance/InheritanceApp.java
@@ -7,7 +7,8 @@ class Cal{
 
     //m Overloading : 동일한 이름의 메소드를 다른 형태(매게변수)로 정의하는 것을 말함
     public int sum(int v1, int v2, int v3){
-        return v1+v2+v3;
+        //return v1+v2+v3;
+        return this.sum(v1,v2)+v3;
     }
 }
 
@@ -17,10 +18,18 @@ class Cal3 extends Cal{
     }
 
     //m 부모 클래스의 기능을 재정의
+    //@Override
+    //public int sum(int v1, int v2) {
+    //    System.out.println("Cal3");
+    //    return v1+v2;
+    //}
+
+    //m 부모 클래스의 메소드를 Override 할 때 그냥 복붙 하면 많은 중복이 일어남
+    //m 계승 발전을 시키는 방법을 이용
     @Override
     public int sum(int v1, int v2) {
         System.out.println("Cal3");
-        return v1+v2;
+        return super.sum(v1,v2);
     }
 }
 
