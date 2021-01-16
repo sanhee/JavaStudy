fbeee691354402b1c49375e92f86369954bf3c16
noel <68936@naver.com>
2021-01-16 21:47:18 +0900

    Overloading 실습 동일한 이름의 메소드를 다른 형태(매게변수)로 정의하는 것을 말함

--- src/CodingEverybody/Inheritance/InheritanceApp.java
+++ src/CodingEverybody/Inheritance/InheritanceApp.java
@@ -4,6 +4,11 @@ class Cal{
     public int sum(int v1, int v2){
         return v1+v2;
     }
+
+    //m Overloading : 동일한 이름의 메소드를 다른 형태(매게변수)로 정의하는 것을 말함
+    public int sum(int v1, int v2, int v3){
+        return v1+v2+v3;
+    }
 }
 
 class Cal3 extends Cal{
@@ -23,6 +28,7 @@ public class InheritanceApp {
     public static void main(String[] args) {
         Cal3 c = new Cal3();
         System.out.println(c.sum(2,1));
+        System.out.println(c.sum(2,1,4));
         System.out.println(c.minus(2,1));
     }
 }
