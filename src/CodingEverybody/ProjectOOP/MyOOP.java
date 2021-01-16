24dda06d7280b9dc10328d3f60f669559945d35a
noel <68936@naver.com>
2021-01-16 14:58:25 +0900

    생성자와 this

--- src/CodingEverybody/ProjectOOP/MyOOP.java
+++ src/CodingEverybody/ProjectOOP/MyOOP.java
@@ -2,15 +2,13 @@ package CodingEverybody.ProjectOOP;
 
 public class MyOOP {
     public static void main(String[] args) {
-        Print p1 = new Print(); // Print 라는 클래스를 복제함
-        p1.delimiter = "----";
+        Print p1 = new Print("----"); // Print 라는 클래스를 복제함
         p1.A();
         p1.A();
         p1.B();
         p1.B();
 
-        Print p2 = new Print(); // Print 라는 클래스를 복제함
-        p2.delimiter = "****";
+        Print p2 = new Print("****"); // Print 라는 클래스를 복제함
         p2.A();
         p2.A();
         p2.B();
--- src/CodingEverybody/ProjectOOP/Print.java
+++ src/CodingEverybody/ProjectOOP/Print.java
@@ -5,14 +5,23 @@ class Print{
     //m 자바) 파일이름 -> 파일 이름의 클래스에서 main 메소드를 찾아서 실행하도록 약속 되어있음.
     //m 클래스 안 변수와 메소드를 멤버라고 한다.
 
-    public static String delimiter = "";
+    public String delimiter = "";
+
+    //m 생성자 함수는 초기에 필요한 값이나, 초기화를 위해 사용함.
+    //m static을 사용하지 않고, 별도의 리턴이 존재하지 않는다.
+    //m this 함수는 인스턴스 변수를 가리키는 것으로 명시적으로 표시해주는 것이 좋다.
+
+    public Print(String delimiter){
+        this.delimiter = delimiter;
+    }
+
     public void A() {
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("A");
         System.out.println("A");
     }
     public void B() {
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("B");
         System.out.println("B");
     }
