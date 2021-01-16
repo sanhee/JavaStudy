e3fd4fb17f98946e4e5f6ad8802a3c290fea2535
noel <68936@naver.com>
2021-01-16 22:36:40 +0900

    상속과 생성자 실습

--- src/CodingEverybody/Inheritance/InheritanceApp.java
+++ src/CodingEverybody/Inheritance/InheritanceApp.java
@@ -1,43 +1,37 @@
 package CodingEverybody.Inheritance;
 
 class Cal{
-    public int sum(int v1, int v2){
-        return v1+v2;
+    int v1, v2;
+    Cal(int v1, int v2){
+        System.out.println("Cal init!!");
+        this.v1 = v1;
+        this.v2 = v2;
     }
-
-    //m Overloading : 동일한 이름의 메소드를 다른 형태(매게변수)로 정의하는 것을 말함
-    public int sum(int v1, int v2, int v3){
-        //return v1+v2+v3;
-        return this.sum(v1,v2)+v3;
+    public int sum(){
+        return this.v1+v2;
     }
 }
 
 class Cal3 extends Cal{
-    public int minus(int v1, int v2){
-        return v1-v2;
+    //m 부모클래스에 생성자 함수를 추가 했더니 에러가 생김!
+    //m 부모를 계승했다면, Cal이 인스턴스로 만들어졌을 때, 반드시 해야하는 일이 생성자에 있음.
+    //m 따라서, 자식 클래스는 생성자 함수를 만들고, 부모 클래스를 호출 해야함.
+    Cal3(int v1, int v2) {
+        super(v1, v2);
+        System.out.println("Cal3 init!!");
     }
-
-    //m 부모 클래스의 기능을 재정의
-    //@Override
-    //public int sum(int v1, int v2) {
-    //    System.out.println("Cal3");
-    //    return v1+v2;
-    //}
-
-    //m 부모 클래스의 메소드를 Override 할 때 그냥 복붙 하면 많은 중복이 일어남
-    //m 계승 발전을 시키는 방법을 이용
-    @Override
-    public int sum(int v1, int v2) {
-        System.out.println("Cal3");
-        return super.sum(v1,v2);
+    public int minus(){
+        return this.v1-v2;
     }
 }
 
 public class InheritanceApp {
     public static void main(String[] args) {
-        Cal3 c = new Cal3();
-        System.out.println(c.sum(2,1));
-        System.out.println(c.sum(2,1,4));
-        System.out.println(c.minus(2,1));
+
+        Cal c = new Cal(2,1);
+        Cal3 c3 = new Cal3(3,1);
+        System.out.println(c3.sum());
+        System.out.println(c3.minus());
+
     }
 }
