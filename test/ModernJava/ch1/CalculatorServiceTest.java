ff2f677270a14e183362da1107a3fa072e535221
noel <68936@naver.com>
2021-01-18 17:04:41 +0900

    전략패턴 적용

--- test/ModernJava/ch1/CalculatorServiceTest.java
+++ test/ModernJava/ch1/CalculatorServiceTest.java
@@ -8,27 +8,27 @@ class CalculatorServiceTest {
 
     @Test
     public void testCalculateAddition() throws Exception{
-        CalculatorService calculatorService = new CalculatorService();
-        final int actual = calculatorService.calculate('+',1,1);
+        CalculatorService calculatorService = new CalculatorService(new Addition());
+        final int actual = calculatorService.calculate(1,1);
         assertEquals(2, actual);
     }
 
     @Test
     public void testCalculateSub() throws Exception{
-        CalculatorService calculatorService = new CalculatorService();
-        final int actual = calculatorService.calculate('-',1,1);
+        CalculatorService calculatorService = new CalculatorService(new Subtraction());
+        final int actual = calculatorService.calculate(1,1);
         assertEquals(2, actual);
     }
     @Test
     public void testCalculateMultiplication() throws Exception{
-        CalculatorService calculatorService = new CalculatorService();
-        final int actual = calculatorService.calculate('*',1,1);
+        CalculatorService calculatorService = new CalculatorService(new Multiplication());
+        final int actual = calculatorService.calculate(1,1);
         assertEquals(2, actual);
     }
     @Test
     public void testCalculateDivision() throws Exception{
-        CalculatorService calculatorService = new CalculatorService();
-        final int actual = calculatorService.calculate('/',1,1);
+        CalculatorService calculatorService = new CalculatorService(new Division());
+        final int actual = calculatorService.calculate(8,2);
         assertEquals(2, actual);
     }
 }
