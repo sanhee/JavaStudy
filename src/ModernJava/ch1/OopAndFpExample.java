398ef8f0de9bb79a8788672adc54bd5d22cbbb73
noel <68936@naver.com>
2021-01-19 02:19:10 +0900

    Ramada Expression

--- src/ModernJava/ch1/OopAndFpExample.java
+++ src/ModernJava/ch1/OopAndFpExample.java
@@ -30,70 +30,17 @@ class Division implements Calculation {
     }
 }
 
-
-class CalculatorService {
-    private final Calculation calculation;
-    private final Calculation calculation2;
-
-
-    public CalculatorService(Calculation calculation, Calculation calculation2) {
-        this.calculation = calculation;
-        this.calculation2 = calculation2;
-    }
-
-    public int calculate(int num1, int num2) {
-        if(num1 > 10 && num2 <num1){
-            return this.calculation.calculate(num1,num2);
-        }
-        else{
-            throw new IllegalArgumentException("Invalid input num1: "+num1+", num2: "+num2);
-        }
-
-    }
-
-    public int compute(int num1, int num2) {
-        if(num1 > 10 && num2 <num1){
-            return this.calculation2.calculate(num1,num2);
-        }
-        else{
-            throw new IllegalArgumentException("Invalid input num1: "+num1+", num2: "+num2);
-        }
-
-    }
-}
-
-
 public class OopAndFpExample{
 
     public static void main(String[] args) {
 
-        final CalculatorService calculatorService = new CalculatorService(new Addition(),new Subtraction());
-
-        /*
-        final int additionResult = calculatorService.calculate(1, 1);
-        System.out.println(additionResult);
-
-        //m 추가 요구 사항으로 뺄셈 기능이 필요하다면?
-        final int subtractionResult = calculatorService.calculate(1, 1);
-        System.out.println(subtractionResult);
-
-        //m 요구사항이 자꾸 늘어나면 코드가 점점 길어짐
-        final int multiplicationResult = calculatorService.calculate(1, 1);
-        System.out.println(multiplicationResult);
-
-        //m ...
-        final int divisionResult = calculatorService.calculate(8, 4);
-        System.out.println(divisionResult);
-
-         */
-
         FpCalculatorService fpCalculatorService =new FpCalculatorService();
 
         //m DI: Dependency Injection, It is Strategy Pattern of Design Pattern
-        System.out.println(" addition: "+fpCalculatorService.calculate(new Addition(),11,4));
-        System.out.println(" subtraction: "+fpCalculatorService.calculate(new Subtraction(),11,1));
-        System.out.println(" Multiplication: "+fpCalculatorService.calculate(new Multiplication(),11,2));
-        System.out.println(" Division: "+fpCalculatorService.calculate(new Division(),20,4));
+        System.out.println(" addition: "+fpCalculatorService.calculate((num1,num2)->{return num1+num2;},11,4));
+        System.out.println(" subtraction: "+fpCalculatorService.calculate((num1,num2)->num1-num2,11,1));
+        System.out.println(" Multiplication: "+fpCalculatorService.calculate((n1,n2)->n1*n2,11,2));
+        System.out.println(" Division: "+fpCalculatorService.calculate((int n1,int n2)->n1/n2,20,4));
 
     }
 }
