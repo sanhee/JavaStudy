6bc9c3c6c32cbd4efcdc638145e2d54280582168
noel <68936@naver.com>
2021-01-19 19:34:02 +0900

    람다,스트림 예제

--- src/ModernJava/ch9/ClosureExample.java
+++ src/ModernJava/ch9/ClosureExample.java
@@ -5,7 +5,6 @@ public class ClosureExample {
     public static void main(String[] args) {
         final int number = 100;
 
-        System.out.println("Anonymous Class");
         final Runnable runnable = new Runnable() { //m 익명함수
             @Override
             public void run() {
@@ -14,10 +13,8 @@ public class ClosureExample {
         };
         runnable.run();
 
-        System.out.println("Lambda");
         Runnable runnable2 = ()-> System.out.println(number);  //m 람다 표현식
         runnable2.run();
-
     }
 
 }
