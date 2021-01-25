bd8e37b46e66943c12a6b2edf534eac86c00475f
noel <68936@naver.com>
2021-01-26 00:05:16 +0900

    주석추가

--- src/etc/CompletableFutureEx1.java
+++ src/etc/CompletableFutureEx1.java
@@ -10,16 +10,16 @@ public class CompletableFutureEx1 {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService executor = Executors.newSingleThreadExecutor(); //m 비동기 task가 수행될 thread를 생성한다.
 
         CompletableFuture
-                .runAsync(()->{
+                .runAsync(()->{ //m runAsync(): 다른 쓰레드에서 비동기식으로 동작할 로직를 선언
                     try{Thread.sleep(1000);} catch(Exception e){};
                     System.out.println("Hello!");
                     try{Thread.sleep(1000);} catch(Exception e){};
                 },executor)
 
-                .thenRun(()->System.out.println("World"));
+                .thenRun(()->System.out.println(" World")); //m .thenRun(): 첫번째 타스크가 완료된 이후에 연속적으로 동작할 로직을 선언
 
         System.out.println("async request is ready.");
 
