6af1c0517af1496d7369a574c8bdb251f9040317
noel <68936@naver.com>
2021-02-06 18:15:14 +0900

    instnaceOf연산자 연습

--- src/JavaStandard/ch07/InstanceofTest.java
+++ src/JavaStandard/ch07/InstanceofTest.java
@@ -3,14 +3,16 @@ package JavaStandard.ch07;
 public class InstanceofTest {
     public static void main(String[] args) {
         FireEngine fe = new FireEngine();
+        Car fe2 = new Car();
 
-        if(fe instanceof FireEngine){
+        //m instanceof 는 참조변수가 참조하고 있는 인스턴스의 실제 타입(클래스)를 알아보는 것.
+        if(fe2 instanceof FireEngine){
             System.out.println("This is a FireEngine instance.");
         }
-        if(fe instanceof Car){
+        if(fe2 instanceof Car){
             System.out.println("This is a Car instance.");
         }
-        if(fe instanceof Object){
+        if(fe2 instanceof Object){
             System.out.println("This is an Object instance");
         }
     }// main close
