0b45c32b26f1fac4f1b19d8671d98ba53311f475
noel <68936@naver.com>
2021-02-19 01:32:48 +0900

    호출 받은 메서드에서 exception을 던질 때, 호출한 메서드에서 처리하는 예제

---
+++ src/JavaStandard/ch08/ExceptionEx16.java
@@ -0,0 +1,24 @@
+package JavaStandard.ch08;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ExceptionEx16 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        try{
+            String fileName = scanner.nextLine();
+            createNewFile(fileName);
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("잘못되었습니다, 다시 입력하세요.");
+        }
+        scanner.close();
+    }
+
+    private static void createNewFile(String fileName) throws Exception {
+        if(fileName == null || fileName.equals("")) throw new Exception("파일이름이 유효하지 않습니다.");
+        File f = new File(fileName);
+        f.createNewFile();
+    }
+}
