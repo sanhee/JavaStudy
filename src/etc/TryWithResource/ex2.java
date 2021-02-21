13a1f71f7cb41dd90aa34151a4684d09999837cc
noel <68936@naver.com>
2021-02-21 22:04:04 +0900

    AutoCloseable을 구현하는 클래스 만들기
    내가 만든 클래스가 try-with-resources으로 자원이 해제되길 원한다면 AutoCloseable을 implements해야 한다.

---
+++ src/etc/TryWithResource/ex2.java
@@ -0,0 +1,25 @@
+package etc.TryWithResource;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        try(CustomResource cr = new CustomResource()){
+            System.out.println("Do something...");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static class CustomResource implements AutoCloseable{
+
+        public void doSomething(){
+            System.out.println("Do something...");
+        }
+
+        @Override
+        public void close() throws Exception {
+            System.out.println("CustomResource.close() is called");
+        }
+    }
+}
+
