7caf67c4b39da31143bb58db69b8b6c501755d95
noel <68936@naver.com>
2021-02-22 22:00:41 +0900

    클론 예제와 공변 반환 예제를 적용해보았다.
    아직 공변성/반공변성에 대해 감이 오질 않지만
    하다보면 알게 되겠지.

---
+++ src/JavaStandard/ch09/CloneEx1.java
@@ -0,0 +1,40 @@
+package JavaStandard.ch09;
+
+public class CloneEx1 {
+     static class Point implements Cloneable{
+        int x, y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        @Override
+        public String toString() {
+            return "Point{" + "x=" + x + ", y=" + y + '}';
+        }
+        @Override
+        public Point clone(){ // 상속관계가 없는 다른 클래스에서 clone을 사용하기 위해서 modifier 를 public으로 변경해주었다.
+            Object obj = null;
+            try{
+                obj = super.clone();
+            }
+            catch (CloneNotSupportedException e){
+                e.printStackTrace();
+            }
+            return (Point)obj;
+        }
+    }
+    public static void main(String[] args) {
+
+        Point original = new Point(3, 5);
+        Point copy = original.clone(); // 복제
+        System.out.println(original);
+        System.out.println(copy);
+
+        System.out.println("==================");
+
+        System.out.println("original.hashCode(): "+ original.hashCode());
+        System.out.println("copy.hashCode(): "+copy.hashCode());
+
+    }
+
+}
