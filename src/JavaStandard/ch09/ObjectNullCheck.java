6e3a076661defc68e637ac19a169e3ec5e584500
noel <68936@naver.com>
2021-03-02 00:52:20 +0900

    Object의 보조 클래스인 Objects 클래스의 requireNonNull()메소드와 toString 메소드를 통해 NULL 을 제어해보았다.

---
+++ src/JavaStandard/ch09/ObjectNullCheck.java
@@ -0,0 +1,25 @@
+package JavaStandard.ch09;
+
+import java.util.Objects;
+
+class test{
+    private String name;
+
+    public test(String name) {
+        //this.name = name;
+
+        this.name = Objects.requireNonNull(name,"어허 NULL 값이다!!");
+
+        System.out.println(this.name);
+    }
+}
+
+public class ObjectNullCheck {
+
+    public static void main(String[] args) {
+        test t = null;
+        Object t3 = t;
+        System.out.println(Objects.toString(t3,"Null 대신 써줄게!"));
+        test t2 = new test(null);
+    }
+}
