42d7aeb3b0460b4ef7ad8e7b7ffcce90f027bbd8
noel <68936@naver.com>
2021-03-01 21:58:22 +0900

    BigInteger 살아있네!

---
+++ src/JavaStandard/ch09/WrapperClass.java
@@ -0,0 +1,10 @@
+package JavaStandard.ch09;
+
+import java.math.BigInteger;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        BigInteger bg = new BigInteger("1221474834242343867867867896709670670670670242666666666666666666666666666666666666666666666633333333333333332222222222222222222222222223333333333333343647");
+        System.out.println(bg);
+    }
+}
