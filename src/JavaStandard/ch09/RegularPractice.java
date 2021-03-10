f11429810ce173229338bbb351d25b605d9ed186
noel <68936@naver.com>
2021-03-11 01:51:22 +0900

    정규식 표현으로 문자 치환하기 예제2

---
+++ src/JavaStandard/ch09/RegularPractice.java
@@ -0,0 +1,25 @@
+package JavaStandard.ch09;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularPractice {
+    public static void main(String[] args) {
+
+        String originString = "Once you see all those animals, you should feel better";
+        String changeWord = "you";
+        StringBuffer sb = new StringBuffer();
+
+        Pattern p = Pattern.compile(changeWord);
+        Matcher matcher = p.matcher(originString);
+
+        while (matcher.find()){
+            matcher.appendReplacement(sb,"***");
+        }
+        matcher.appendTail(sb);
+
+        System.out.println(sb.toString());
+
+
+    }
+}
